8e2199ccdffa84f2051030d5c15f550af3edf2e4
Tony Otite <casht40@hotmail.com>
2022-11-20 14:38:38 -0500

    react budget app changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
+import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 {/* Add the code here to add the components          */}
+                <Budget />
+                <Remaining />
+                <ExpenseItem />
+                <ExpenseList />
+                <ExpenseTotal />
+                <AllocationForm />
+
             </div>
         </AppProvider>
     );
